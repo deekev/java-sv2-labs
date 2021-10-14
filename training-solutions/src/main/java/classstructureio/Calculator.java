ff76e8af025a90c99026d426754eb8b085e1f833
deekev <deekev@gmail.com>
2021-10-14 20:30:37 +0200

    Calculator kész

---
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,21 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a number:");
+        int firstNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Write another number:");
+        int secondNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Your numbers are:");
+        System.out.println(""+firstNumber+" + "+secondNumber);
+        System.out.println(firstNumber+secondNumber);
+    }
+}
