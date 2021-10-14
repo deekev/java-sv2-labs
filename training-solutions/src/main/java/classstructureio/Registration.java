912e07944de653a02e4ad845c40afb370f15f6dc
deekev <deekev@gmail.com>
2021-10-14 20:52:36 +0200

    Registration kész

---
+++ training-solutions/src/main/java/classstructureio/Registration.java
@@ -0,0 +1,20 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write your name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Write your e-mail address:");
+        String email = scanner.nextLine();
+
+        System.out.println();
+        System.out.println("You have successfully registered.");
+        System.out.println("Name:" + "" + name);
+        System.out.println("E-mail address:" + "" + email);
+    }
+}
