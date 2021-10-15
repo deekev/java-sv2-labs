2254dfd7099d4589eccdb0214404ee3f751e29b7
deekev <deekev@gmail.com>
2021-10-15 14:26:09 +0200

    Second push

--- intro/src/main/java/introgit/HelloGit.java
+++ intro/src/main/java/introgit/HelloGit.java
@@ -2,6 +2,6 @@ package introgit;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello IDEA and Git!");
     }
 }
