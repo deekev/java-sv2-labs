8bd5baf15e0ccd7aeea2539790e06a199f1d5e2b
deekev <deekev@gmail.com>
2021-10-19 20:57:13 +0200

    String osztály kész

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+import java.util.Locale;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String lowString = "Hello World!";
+        System.out.println(lowString.toUpperCase());
+        System.out.println("Hello World!".toUpperCase());
+    }
+}
