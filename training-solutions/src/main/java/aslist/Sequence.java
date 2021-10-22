4fc3331669b70baecb96271aff94a1cf78f25f31
deekev <deekev@gmail.com>
2021-10-22 23:10:36 +0200

    számsorozat helyesbítés

--- training-solutions/src/main/java/aslist/Sequence.java
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -9,11 +9,13 @@ public class Sequence {
     public static void main(String[] args) {
 
         List<Integer> elements = new ArrayList<>(Arrays.asList(2, 3));
+        int numberOfNewListItems = 5;
         int index = elements.size();
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < numberOfNewListItems; i++){
             elements.add(index, elements.get(index-2) * elements.get(index-1));
             index++;
         }
+        System.out.println(elements.size());
         for (int i: elements){
             System.out.print(i + ", ");
         }
