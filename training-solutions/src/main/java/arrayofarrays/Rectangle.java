6d82f960fe5d2a4831a275f687e9d1ea5367da45
deekev <deekev@gmail.com>
2021-10-22 14:33:47 +0200

    négyszögmátrix

---
+++ training-solutions/src/main/java/arrayofarrays/Rectangle.java
@@ -0,0 +1,24 @@
+package arrayofarrays;
+
+public class Rectangle {
+
+    public int[][] rectangleMatrix(int size){
+        int[][] array = new int [size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = i;
+                System.out.print(array[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        return array;
+    }
+
+
+    public static void main(String args[]) {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.rectangleMatrix(6);
+    }
+}
