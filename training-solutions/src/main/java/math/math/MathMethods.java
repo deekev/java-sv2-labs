9a183a2dec47a17a5fe24b9d61788077e8ad7be8
deekev <deekev@gmail.com>
2021-10-23 18:24:14 +0200

    Math

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,22 @@
+package math.math;
+
+public class MathMethods {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.max(13, 8));
+        System.out.println(Math.min(29, 18));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(4.49986));
+        System.out.println(Math.abs(-28.3));
+        System.out.println(Math.negateExact(1));
+        System.out.println(Math.addExact(756, 534));
+        System.out.println(Math.subtractExact(78965, 54892));
+        System.out.println(Math.multiplyExact(549, 34));
+        System.out.println(Math.pow(5.23, 7.48));
+        System.out.println(Math.incrementExact(6));
+        System.out.println(Math.decrementExact(7496));
+        System.out.println(Math.random());
+    }
+}
