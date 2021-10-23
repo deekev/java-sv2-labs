026d309a9fef5bef2af9d00e2423d3cef968526c
deekev <deekev@gmail.com>
2021-10-23 16:08:32 +0200

    stars

---
+++ training-solutions/src/main/java/debug/Star.java
@@ -0,0 +1,29 @@
+package debug;
+
+public class Star {
+
+    public void writeStars(int[] numbers) {
+        int prev = 0;
+        for (int i = 0; i < numbers.length; i++) {
+        //  int prev = 0;
+            int stars = numbers[i] - prev;
+            printStars(stars);
+            prev = numbers[i];
+            System.out.println();
+        }
+    }
+
+    private void printStars(int stars) {
+        for (int i = 1; i <= stars; i++) {
+            System.out.print("*");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Star star = new Star();
+        int[] numberOfStars = {1, 3, 5, 10, 12};
+        star.writeStars(numberOfStars);
+    }
+}
