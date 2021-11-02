90459104f53fcc3e8a82f34c870f93046d72b694
deekev <deekev@gmail.com>
2021-11-02 20:01:53 +0100

    Hibás teszt (teszt)

--- training-solutions/src/test/java/introjunit/GentlemanTest.java
+++ training-solutions/src/test/java/introjunit/GentlemanTest.java
@@ -9,15 +9,15 @@ public class GentlemanTest {
 
     @Test
     public void testSayHello() {
-        assertThat(new Gentleman().sayHello("John Doe"), equalTo("Hello John Doe!"));
+        assertThat(new Gentleman().sayHello("John Doe"), equalTo("HelloJohn Doe!"));
     }
 }
 
 /*
-Rontsd el először a programot, hogy hibás üzenetet adjon vissza!
+Rontsd el a tesztesetet, hogy hibás legyen, amire ellenőriz!
 java.lang.AssertionError:
-Expected: "Hello John Doe!"
-     but: was "HelloJohn Doe!"
-Expected :Hello John Doe!
-Actual   :HelloJohn Doe!
+Expected: "HelloJohn Doe!"
+     but: was "Hello John Doe!"
+Expected :HelloJohn Doe!
+Actual   :Hello John Doe!
  */
