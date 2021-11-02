ff1469242aef9ccd67a5a8be91935a3f1587371d
deekev <deekev@gmail.com>
2021-11-02 22:19:43 +0100

    Páros számok

---
+++ training-solutions/src/test/java/junit5assert/NumbersTest.java
@@ -0,0 +1,16 @@
+package junit5assert;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class NumbersTest {
+
+    @Test
+    void testGetEvenNumbers() {
+        Numbers numbers = new Numbers();
+        int[] numbersToTest = {4, 3, 5, 6, 2, 7};
+        int[] numbersExpected = {4, 0, 0, 6, 2, 0};
+        assertArrayEquals(numbersExpected, numbers.getEvenNumbers(numbersToTest));
+    }
+}
