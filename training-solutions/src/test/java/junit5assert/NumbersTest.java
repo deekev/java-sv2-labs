7879b6f873a1b35f85d634d470ae4e2b3d560d44
deekev <deekev@gmail.com>
2021-11-03 19:47:14 +0100

    remove public

--- training-solutions/src/test/java/junit5assert/NumbersTest.java
+++ training-solutions/src/test/java/junit5assert/NumbersTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class NumbersTest {
+class NumbersTest {
 
     @Test
     void testGetEvenNumbers() {
--- training-solutions/src/test/java/junit5fixture/WordsTest.java
+++ training-solutions/src/test/java/junit5fixture/WordsTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WordsTest {
+class WordsTest {
 
     Words words;
 
