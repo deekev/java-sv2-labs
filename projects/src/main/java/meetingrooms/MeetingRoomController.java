8abb11027cc2be449a71cb13e8bab4d66d4c3122
deekev <deekev@gmail.com>
2021-11-05 08:22:29 +0100

    osztályok

---
+++ projects/src/main/java/meetingrooms/MeetingRoomController.java
@@ -0,0 +1,12 @@
+package meetingrooms;
+
+public class MeetingRoomController {
+
+    private Office office;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ projects/src/main/java/meetingrooms/Office.java
@@ -0,0 +1,9 @@
+package meetingrooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Office {
+
+    private List<MeetingRoom> meetingRooms = new ArrayList<>();
+}
