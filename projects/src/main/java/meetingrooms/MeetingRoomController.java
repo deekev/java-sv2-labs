5532ee36fe0b09ab6701c4766b650e954fcbf154
deekev <deekev@gmail.com>
2021-11-09 20:12:46 +0100

    metódusok kész

--- projects/src/main/java/meetingrooms/MeetingRoomController.java
+++ projects/src/main/java/meetingrooms/MeetingRoomController.java
@@ -1,12 +1,113 @@
 package meetingrooms;
 
+import java.util.Scanner;
+
 public class MeetingRoomController {
 
-    private Office office;
+    Scanner sc = new Scanner(System.in);
+
+    private Office office = new Office();
+
+    private final String menu =
+                    "1. Tárgyaló rögzítése\n" +
+                    "2. Tárgyalók sorrendben\n" +
+                    "3. Tárgyalók visszafele sorrendben\n" +
+                    "4. Minden második tárgyaló\n" +
+                    "5. Területek\n" +
+                    "6. Keresés pontos név alapján\n" +
+                    "7. Keresés névtöredék alapján\n" +
+                    "8. Keresés terület alapján\n" +
+                    "9. Kilépés";
+
+    public void printMenu() {
+        System.out.print(menu);
+        System.out.println();
+    }
+
+    public void readOffice() {
+        System.out.println("Adja meg a tárgyaló paramétereit: ");
+        System.out.println("Név:");
+        String name = sc.nextLine();
+        System.out.println("Hosszúság (m-ben): ");
+        int length = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Szélesség (m-ben): ");
+        int width = sc.nextInt();
+        sc.nextLine();
+        System.out.println();
+
+        MeetingRoom meetingRoom = new MeetingRoom(name, length, width);
+        office.addMeetingRoom(meetingRoom);
+    }
+
+    public void runMenu() {
+        printMenu();
+        System.out.println();
+        System.out.println("Adja meg a kiválasztott menüpont számát: ");
+        int numberOfMenu = sc.nextInt();
+        sc.nextLine();
+        System.out.println();
+        selectMenu(numberOfMenu);
+    }
+
+    private void selectMenu(int number) {
+        switch (number) {
+            case 1:
+                readOffice();
+                runMenu();
+                break;
+
+            case 2:
+                office.printNames();
+                runMenu();
+                break;
+
+            case 3:
+                office.printNamesReverse();
+                runMenu();
+                break;
+
+            case 4:
+                office.printEvenNames();
+                runMenu();
+                break;
+
+            case 5:
+                office.printAreas();
+                runMenu();
+                break;
+
+            case 6:
+                System.out.println("Adja meg a tárgyaló nevét:");
+                String name = sc.nextLine();
+                office.printMeetingRoomsWithName(name);
+                runMenu();
+                break;
+
+            case 7:
+                System.out.println("Adja meg a szórészletet:");
+                String part = sc.nextLine();
+                office.printMeetingRoomsContains(part);
+                runMenu();
+                break;
+
+            case 8:
+                System.out.println("Adja meg a tárgyaló területét:");
+                int area = sc.nextInt();
+                sc.nextLine();
+                office.printAreasLargerThan(area);
+                runMenu();
+                break;
+
+            case 9: break;
+        }
+    }
 
 
 
     public static void main(String[] args) {
 
+        MeetingRoomController meetingRoomController = new MeetingRoomController();
+        meetingRoomController.runMenu();
     }
 }
--- projects/src/main/java/meetingrooms/Office.java
+++ projects/src/main/java/meetingrooms/Office.java
@@ -6,4 +6,71 @@ import java.util.List;
 public class Office {
 
     private List<MeetingRoom> meetingRooms = new ArrayList<>();
+
+    public void addMeetingRoom (MeetingRoom meetingRoom) {
+        meetingRooms.add(meetingRoom);
+    }
+
+    public void printNames() {
+        for (MeetingRoom actual: meetingRooms) {
+            System.out.println(actual.getName());
+        }
+        System.out.println();
+    }
+
+    public void printNamesReverse() {
+        for (int i = meetingRooms.size() - 1; i >= 0; i--) {
+            System.out.println(meetingRooms.get(i).getName());
+        }
+        System.out.println();
+    }
+
+    public void printEvenNames() {
+        for (int i = 0; i < meetingRooms.size(); i += 2) {
+            System.out.println(meetingRooms.get(i).getName());
+        }
+        System.out.println();
+    }
+
+    public void printAreas() {
+        for (MeetingRoom actual: meetingRooms) {
+            System.out.printf("név: %s, hosszúság: %d m, szélesség: %d m , terület: %d m2",
+                    actual.getName(), actual.getLength(), actual.getWidth(), actual.getArea());
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void printMeetingRoomsWithName(String name) {
+        for (MeetingRoom actual: meetingRooms) {
+            if (name.equals(actual.getName())){
+                System.out.printf("hosszúság: %d m, szélesség: %d m , terület: %d m2",
+                         actual.getLength(), actual.getWidth(), actual.getArea());
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+    public void printMeetingRoomsContains(String part) {
+        for (MeetingRoom actual: meetingRooms) {
+            if (actual.getName().toLowerCase().contains(part.toLowerCase())){
+                System.out.printf("hosszúság: %d m, szélesség: %d m , terület: %d m2",
+                        actual.getLength(), actual.getWidth(), actual.getArea());
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+    public void printAreasLargerThan(int area) {
+        for (MeetingRoom actual: meetingRooms) {
+            if (actual.getArea() > area) {
+                System.out.printf("név: %s, hosszúság: %d m, szélesség: %d m , terület: %d m2",
+                        actual.getName(), actual.getLength(), actual.getWidth(), actual.getArea());
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
 }
