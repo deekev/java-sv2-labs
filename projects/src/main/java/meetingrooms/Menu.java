7bedc2290239d74f2f72797075dd528782e87996
deekev <deekev@gmail.com>
2021-11-11 22:33:45 +0100

    StringBuilder

--- projects/src/main/java/meetingrooms/Menu.java
+++ projects/src/main/java/meetingrooms/Menu.java
@@ -3,15 +3,15 @@ package meetingrooms;
 public class Menu {
 
     private final StringBuilder menu = new StringBuilder(
-            "1. Tárgyaló rögzítése\n" +
-                    "2. Tárgyalók sorrendben\n" +
-                    "3. Tárgyalók visszafele sorrendben\n" +
-                    "4. Minden második tárgyaló\n" +
-                    "5. Területek\n" +
-                    "6. Keresés pontos név alapján\n" +
-                    "7. Keresés névtöredék alapján\n" +
-                    "8. Keresés terület alapján\n" +
-                    "9. Kilépés");
+            "1. Tárgyaló rögzítése\n")
+            .append("2. Tárgyalók sorrendben\n")
+            .append("3. Tárgyalók visszafele sorrendben\n")
+            .append("4. Minden második tárgyaló\n")
+            .append("5. Területek\n")
+            .append("6. Keresés pontos név alapján\n")
+            .append("7. Keresés névtöredék alapján\n")
+            .append("8. Keresés terület alapján\n")
+            .append("9. Kilépés");
 
     public StringBuilder getMenu() {
         return menu;
