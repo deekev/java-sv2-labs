52cc8040cb87a7be4154f16dea8567162ecd995b
deekev <deekev@gmail.com>
2021-11-15 23:06:43 +0100

    lövegcső

---
+++ training-solutions/src/main/java/introexceptionthrowjunit5/Tank.java
@@ -0,0 +1,22 @@
+package introexceptionthrowjunit5;
+
+public class Tank {
+
+    private int angle;
+
+    public void modifyAngle(int angleNumber) {
+        angle += angleNumber;
+
+        if (angle > 360) {
+            angle -= 360;
+        }
+
+        if (angle > 80 && angle < 280) {
+            throw new IllegalArgumentException("A fordulás szöge nem eshet 80° és 280° közé!");
+        }
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+}
---
+++ training-solutions/src/test/java/introexceptionthrowjunit5/TankTest.java
@@ -0,0 +1,25 @@
+package introexceptionthrowjunit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TankTest {
+
+    @Test
+    void testModifyAngle() {
+        Tank tank = new Tank();
+        tank.modifyAngle(80);
+        assertEquals(80, tank.getAngle());
+        tank.modifyAngle(280);
+        assertEquals(360, tank.getAngle());
+    }
+
+    @Test
+    void testModifyAngleException() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Tank().modifyAngle(197));
+        assertEquals("A fordulás szöge nem eshet 80° és 280° közé!", exception.getMessage());
+    }
+}
